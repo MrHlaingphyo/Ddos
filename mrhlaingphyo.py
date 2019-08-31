79f023fe12bb5fca375dd8c49ca64a7df27605f6
MrHlaingphyo <54358004+MrHlaingphyo@users.noreply.github.com>
2019-08-31 10:13:53 +0630

    Update mrhlaingphyo.py

--- mrhlaingphyo.py
+++ mrhlaingphyo.py
@@ -58,25 +58,25 @@ def logo():
                     ##########################      #####
                     ###  ###################           \033[33m##\033[31m
                     ##    ###############
-                    \033[33m#\033[31m     ##  ##########   \033[33mMr \033[31m.\033[33m Bat\033[31m
+                    \033[33m#\033[31m     ##  ##########   \033[33mMr \033[31m.\033[33m Hlaingphyo\033[31m
                               ##   ####
                                     ###
                                  \033[33m    ##\033[31m
-          \033[33m-by \033[31mS\033[33mutariya \033[31mP\033[33marixit\033[31m        \033[33m#\033[31m
-\n\t     \033[31m\033[1m[_\033[33mIP Flooder\033[31m_]
+          \033[33m-by \033[31mS\033[33mhlaingphyo\033[31mP\033[33mthakhinGyi\033[31m        \033[33m#\033[31m
+\n\t     \033[31m\033[1m[_\033[33mWebsite Attack\033[31m_]
  \n\033[0m\033[1m
-\t \033[33m\033[1m[-] \033[0m\033[1mPlatform : \033[33m\033[1mAndroid Termux
-\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mName     : \033[33m\033[1mMr.Bat
-\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mSite     : \033[33mwww.bhai4you.blogspot.com
-\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mCoded by :\033[1m \033[33m[  \033[32m\033[1mParixit \033[33m ]
-\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mSec.Code : \033[33m\033[1m8h4i\033[0m
-\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mDate     : \033[33m\033[1m10-Dec-17\033[0m
+\t \033[33m\033[1m[-] \033[0m\033[1mCreator : \033[33m\033[1mMr Hlaing phyo
+\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mName     : \033[33m\033[1mMr.Hlaing phyo Ddos
+\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mSite     : \033[33mwww.hlaingphyoaung.weebly.com
+\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mCoded by :\033[1m \033[33m[  \033[32m\033[1mHlaing Phyo \033[33m ]
+\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mSec.Code : \033[33m\033[1mMr White\033[0m
+\t \033[1m\033[33m\033[1m[-] \033[0m\033[1mDate     : \033[33m\033[1m8/31/2019\033[0m
   """)
 
 
 logo()
 
-print("\n\n\n\t\t\033[32m\033[1mMr.Bat")
+print("\n\n\n\t\t\033[32m\033[1mMr.HlaingPhyo")
 print("\033[33m\033[1m       UDP Port Flooder By \033[31m8h4i\033[0m")
 
 attack = raw_input("\n\n\n\033[1mWebsite or IP ==> \033[33m")
@@ -98,7 +98,7 @@ while True:
                 break
 print("\n")
 print("\n\033[31m\033[1mFlooding Completed...:D\033[33m\n")
-print("Mr.Bat Attack has completed on \033[32m%s\033[33m for \033[32m%s\033[33m seconds by Sutariya Parixit." % (attack, duration))
+print("Mr.Hlaing Phyo Attack has completed on \033[32m%s\033[33m for \033[32m%s\033[33m seconds by Sutariya Parixit." % (attack, duration))
 
 def connect(i):
     try:
